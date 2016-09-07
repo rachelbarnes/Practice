3bad75d15e63fae5dff17e978f9c80cbac9567ec
Rachel Barnes <rachelbarnesjn116@yahoo.com>
2016-09-07 18:09:04 -0400

    Making changes in notes in IsUniqueClass

--- Practice/IsUnique.cs
+++ Practice/IsUnique.cs
@@ -10,7 +10,14 @@ namespace Practice
     [TestFixture]
     public class IsUniqueClass
     {
-        public bool IsUnique(string AmIUnique)
+        /*determine if all the letters in a string are unique
+        
+        I have 2 methods here: 
+            1. use a dictionary to store keys for each new character and aggregating the number of times it is in the string by a counter as the value
+            2. put all the chars in a list and determine if a char is a duplciate list element
+        */ 
+
+        public bool IsUniqueDictionary(string AmIUnique)
         {
             var dict = new Dictionary<char, int>();
             foreach (var ch in AmIUnique)
@@ -25,7 +32,7 @@ namespace Practice
             }
             return true;
         }
-        public bool IsUniqueAgain(string AmIUnique)
+        public bool IsUniqueList(string AmIUnique)
         {
             var list = new List<char>();
             foreach (var ch in AmIUnique)
@@ -38,20 +45,16 @@ namespace Practice
             return true;
         }
 
-
-
-
-
-
-
+        //tests 
         public class IsUniqueTests
         {
+            //testing the first method
             [Test]
             public void IsUnique1()
             {
                 var myClass = new IsUniqueClass();
                 var expected = true;
-                var actual = myClass.IsUnique("Another");
+                var actual = myClass.IsUniqueDictionary("Another");
                 Assert.AreEqual(expected, actual);
             }
             [Test]
@@ -59,16 +62,16 @@ namespace Practice
             {
                 var myClass = new IsUniqueClass();
                 var expected = false;
-                var actual = myClass.IsUnique("Extravagant");
+                var actual = myClass.IsUniqueDictionary("Extravagant");
                 Assert.AreEqual(expected, actual);
             }
-            //testing the second method, IsUniqueAgain
+            //testing the second method
             [Test]
             public void IsUnique3()
             {
                 var myClass = new IsUniqueClass();
                 var expected = true;
-                var actual = myClass.IsUniqueAgain("Share");
+                var actual = myClass.IsUniqueList("Share");
                 Assert.AreEqual(expected, actual);
             }
             [Test]
@@ -76,16 +79,9 @@ namespace Practice
             {
                 var myClass = new IsUniqueClass();
                 var expected = false;
-                var actual = myClass.IsUniqueAgain("Kitty");
-                Assert.AreEqual(expected, actual);
-            }
-            [Test]
-            public void LookingAtThePalPerm()
-            {
-                var myClass = new IsUniqueClass();
-                var expected = true;
-                var actual = myClass.PalPerm("mlkthtmlk");
+                var actual = myClass.IsUniqueList("Kitty");
                 Assert.AreEqual(expected, actual);
             }
         }
     }
+}
